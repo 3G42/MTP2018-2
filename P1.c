15d1b2bbb7f463e74043d92493577130009fc1ea
3G42 <42394318+3G42@users.noreply.github.com>
2018-09-08 13:18:34 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int est = 0,i;
+    char num[256];
+
+    printf("Informe um número em binario para que eu possa verificar se eh um multiplo de 3\n");
+    scanf("%s",&num);
+
+    for (i = 0; num[i]!='\0' ; i++ )
+    {
+        if(est == 0 && num[i]=='1')
+        {
+            est = 1;
+        }
+        else if(est == 1)
+        {
+            if(num[i]=='1')
+            {
+
+                est = 0;
+
+            }
+            else if(num[i]=='0')
+            {
+                est = 2;
+            }
+        }
+        else if(est == 2 && num[i]=='0')
+        {
+            est = 1;
+        }
+    }
+    printf("%s ",num);
+
+    if(est==0)
+    {
+        printf(" eh multiplo de 3");
+    }
+    else
+    {
+        printf(" nao eh");
+    }
+    return 0;
+}
